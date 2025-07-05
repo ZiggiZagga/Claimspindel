699b7303cf54f61d65f009faab754dabcbfcd3a1
ZiggiZagga <75372480+ZiggiZagga@users.noreply.github.com>
2025-07-05 14:39:36 +0000

    added ComponentScan

--- src/main/java/com/ironbucket/claimspindel/GatewayApp.java
+++ src/main/java/com/ironbucket/claimspindel/GatewayApp.java
@@ -4,7 +4,14 @@ package com.ironbucket.claimspindel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan(
+		{			
+			"com.ironbucket.pactumscroll",
+			"com.ironbucket.claimspindel"
+		}
+	)
 @SpringBootApplication
 @EnableDiscoveryClient
 class GatewayApp {
